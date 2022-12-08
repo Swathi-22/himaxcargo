498e9dcf56dc5a250730b051f03492d2d92bd279
Swathi-22 <mkswathisuresh@gmail.com>
2022-12-08 09:53:39 +0530

    Project Initialized

---
+++ web/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name='web'
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('about/', views.about,name='about'),
+    path('services/', views.services,name='services'),
+    path('updates/', views.updates,name='updates'),
+    path('updates_details/', views.updates_details,name='updates_details'),
+    path('contact/', views.contact,name='contact'),
+]
---
+++ web/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context = {"is_index":True}
+    return render(request,'web/index.html',context)
+
+
+def about(request):
+    context = {"is_about":True}
+    return render(request,'web/about.html',context)
+
+
+def services(request):
+    context = {"is_service":True}
+    return render(request,'web/service.html',context)
+
+
+def updates(request):
+    context = {"is_updates":True}
+    return render(request, 'web/blog.html',context)
+
+
+def updates_details(request):
+    context = {}
+    return render(request, 'web/blog-details.html',context)
+
+
+def contact(request):
+    context = {"is_contact":True}
+    return render(request, 'web/contact.html',context)
