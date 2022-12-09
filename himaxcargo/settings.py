b724bb8e98588367ea84b44890eb9fe075bbaf13
Swathi-22 <mkswathisuresh@gmail.com>
2022-12-09 17:07:26 +0530

    Dynamic started

--- himaxcargo/settings.py
+++ himaxcargo/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'web',
+    'versatileimagefield',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +62,21 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+VERSATILEIMAGEFIELD_SETTINGS = {
+    "cache_length": 2592000,
+    "cache_name": "versatileimagefield_cache",
+    "jpeg_resize_quality": 70,
+    "sized_directory_name": "__sized__",
+    "filtered_directory_name": "__filtered__",
+    "placeholder_directory_name": "__placeholder__",
+    "create_images_on_demand": True,
+    "image_key_post_processor": None,
+    "progressive_jpeg": False,
+}
+
 
 
 
--- web/models.py
+++ web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from versatileimagefield.fields import PPOIField
+from versatileimagefield.fields import VersatileImageField
 # Create your models here.
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100)
+    image = image = VersatileImageField("Image", upload_to="Banner/", ppoi_field="ppoi")
+    ppoi = PPOIField("Image PPOI")
+
+    class Meta:
+        verbose_name_plural = "Banner"
+
+    def __str__(self):
+        return str(self.title)
+
+class Service(models.Model):
+    title = models.CharField(max_length=100)
